ced11ecd05c178f0a4e6c43410e272e417c63582
Your Name <you@example.com>
2020-06-06 16:07:52 +0800

    新增條件四，可選擇條件

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Horus
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags -w main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags -w horus.go
 
 ## 條件
 1. MA1 > MA2 > MA3
 2. MV1 > MV2 > MV3
 3. D天內，每日的成交量不低於X張
+4. 最後Z天總和的成交量大於Z天前(C天)總和的成交量G%
 
 ## config.yml 參數設定
 1. MA1: k線1
@@ -16,4 +17,11 @@ CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags -w main.go
 7. MAX: 查詢最大天數
 8. DAYS: 條件三的D
 9. LOT: 條件三的X
-10. FILTER: 忽略最近的天數，不含假日
+10. Z: 條件四的Z
+11. C: 條件四的C
+12. G: 條件四的G
+13. Role1: 條件1 true or false
+14. Role2: 條件2 true or false
+15. Role3: 條件3 true or false
+16. Role4: 條件4 true or false
+17. FILTER: 忽略最近的天數，不含假日
--- horus.go
+++ horus.go
@@ -27,16 +27,23 @@ var config = &conf{}
 var client = &http.Client{}
 
 type conf struct {
-	MA1  float64 `yaml:"MA1"`
-	MA2  float64 `yaml:"MA2"`
-	MA3  float64 `yaml:"MA3"`
-	MV1  float64 `yaml:"MV1"`
-	MV2  float64 `yaml:"MV2"`
-	MV3  float64 `yaml:"MV3"`
-	MAX  int     `yaml:"MAX"`
-	DAYS int     `yaml:"DAYS"`
-	LOT  float64 `yaml:"LOT"`
-	FILTER int     `yaml:"FILTER"`
+	MA1   float64 `yaml:"MA1"`
+	MA2   float64 `yaml:"MA2"`
+	MA3   float64 `yaml:"MA3"`
+	MV1   float64 `yaml:"MV1"`
+	MV2   float64 `yaml:"MV2"`
+	MV3   float64 `yaml:"MV3"`
+	MAX   int     `yaml:"MAX"`
+	DAYS  int     `yaml:"DAYS"`
+	LOT   float64 `yaml:"LOT"`
+	Z 	  int `yaml:"Z"`
+	C 	  int `yaml:"C"`
+	G     float64 `yaml:"G"`
+	Role1 bool    `yaml:"Role1"`
+	Role2 bool    `yaml:"Role2"`
+	Role3 bool    `yaml:"Role3"`
+	Role4 bool    `yaml:"Role4"`
+	FILTER int    `yaml:"FILTER"`
 }
 
 func (c *conf) getConf() *conf {
@@ -71,17 +78,29 @@ func main() {
 				MV := strings.Split(array1[5], ",")
 				MV = MV[:len(MV)-config.FILTER]
 
-				MA1 := AverageCalculation(MA, config.MA1)
-				MA2 := AverageCalculation(MA, config.MA2)
-				MA3 := AverageCalculation(MA, config.MA3)
-
-				MV1 := AverageCalculation(MV, config.MV1)
-				MV2 := AverageCalculation(MV, config.MV2)
-				MV3 := AverageCalculation(MV, config.MV3)
-
-				if MA1 > MA2 && MA2 > MA3 && MV1 > MV2 && MV2 > MV3 && Condition3(MV) == true {
-					fmt.Println(stockNumber + " " + stockName)
+				if config.Role1 {
+					if Condition1(MA) != true{
+						goto BREAK
+					}
 				}
+				if config.Role2 {
+					if Condition2(MV) != true{
+						goto BREAK
+					}
+				}
+				if config.Role3 {
+					if Condition3(MV) != true{
+						goto BREAK
+					}
+				}
+				if config.Role4 {
+					if Condition4(MV) != true{
+						goto BREAK
+					}
+				}
+
+				fmt.Println(stockNumber + " " + stockName)
+				BREAK:
 			}
 		}
 	}
@@ -133,6 +152,28 @@ func AverageCalculation(data []string, average float64) float64 {
 	return value
 }
 
+// Condition1 MA1 > MA2 > MA3
+func Condition1(MA []string) bool {
+	MA1 := AverageCalculation(MA, config.MA1)
+	MA2 := AverageCalculation(MA, config.MA2)
+	MA3 := AverageCalculation(MA, config.MA3)
+	if MA1 > MA2 && MA2 > MA3 {
+		return true
+	}
+	return false
+}
+
+// Condition2 MV1 > MV2 > MV3
+func Condition2(MV []string) bool {
+	MV1 := AverageCalculation(MV, config.MV1)
+	MV2 := AverageCalculation(MV, config.MV2)
+	MV3 := AverageCalculation(MV, config.MV3)
+	if MV1 > MV2 && MV2 > MV3 {
+		return true
+	}
+	return false
+}
+
 // Condition3 D天內，每日的成交量不低於X張
 func Condition3(data []string) bool {
 	value := 0
@@ -144,6 +185,27 @@ func Condition3(data []string) bool {
 			}
 		}
 		if value == config.DAYS {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+// Condition4 最後Z天總和的成交量大於Z天前(C天)總和的成交量G%
+func Condition4(data []string) bool {
+	value1 := 0.0
+	value2 := 0.0
+	if config.Z + config.C <= len(data) {
+		for i := 0; i < config.Z; i++ {
+			v, _ := strconv.ParseFloat(data[len(data)-i-1], 64)
+			value1 = value1 + v
+		}
+		for i := config.Z; i < config.Z + config.C; i++ {
+			v, _ := strconv.ParseFloat(data[len(data)-i-1], 64)
+			value2 = value2 + v 
+		}
+		if value1 > value2 * 0.01 * (config.G + 100){
 			return true
 		}
 		return false
